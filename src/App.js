1d9de22da197ccbfd5923501df1d378c9ce5e986
osaze <osareniyeosazee@gmail.com>
2021-03-26 14:37:43 +0100

    tru profile update

--- src/App.js
+++ src/App.js
@@ -14,17 +14,18 @@ export default function App() {
   const [reciept, setRecipt] = useState([])
   const [alert, setAlert] = useState("")
 
-    const APP_KEY="ac23f2ffd7b728c4eb1d8ffa8fb5b997"
+    const APP_KEY="ac23f2ffd7b728c4eb1d8ffa8fb5b997	"
     const APP_id="4217f819"
-    const url=`https://api.edamam.com/search?q=${query}&app_id=${APP_id}&app_key=${APP_KEY}`
+    const url=`https://api.edamam.com/search?q=chicken&app_id=${APP_id}&app_key=${APP_KEY}`
 
-    const getData = async ()=>{
+    const getData = async () =>{
       if(query !== ""){
         const result =await Axios.get(url)
-        if(!result.data.more){
-          return setAlert("no food with such name")
-        }
-        setRecipt(result.data.hits)
+        console.log(result)
+       if(!result.data){
+        return setAlert("no food with such name")
+       }
+        setRecipt(result)
      console.log(result)
      setAlert()
      setQuery("");
